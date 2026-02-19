06645273489efa36cdb90ce1c788f2dcb3f49070
Manuradha Singh <your_email@example.com>
2026-02-19 12:08:52 +0530

    few changes for deployment

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const fileHandler = require('./modules/fileHandler');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
